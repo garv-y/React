178af2bdc7c0c4ebed79b53910754aa54dcc4b24
garv-y <126312317+garv-y@users.noreply.github.com>
2025-07-01 15:33:07 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+
+function NameList() {
+  const [list, setList] = useState(["garv", "jitu", "kamal"]);
+  const [name, setName] = useState(() => "");
+
+  const onAddName = () => {
+    setList([...list, name]);
+    setName("");
+  };
+
+  return (
+    <div>
+      <ul>
+        {list.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onAddName}>Add Name</button>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(10);
+
+  function addOne() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <h1>Welcome to React</h1>
+      <button onClick={addOne}>Count = {count}</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <NameList />
+    </div>
+  );
+}
+
+export default App;
